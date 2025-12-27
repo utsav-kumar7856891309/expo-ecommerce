b3113e49da01e7b4aef1403e2ff27d19db493074
utsav-kumar7856891309 <15102004utsavkumar@gmail.com>
2025-12-27 23:27:22 +0530

    project setup

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ClerkProvider } from "@clerk/clerk-react";
+import { BrowserRouter } from "react-router";
 
-createRoot(document.getElementById('root')).render(
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+          <App />
+      </ClerkProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,15 @@
 import express from "express";
 import path from "path";
-
+import { clerkMiddleware } from "@clerk/express";
 import { ENV } from "./config/env.js";
+import { serve } from "inngest/express";
+import { connectDB } from "./config/db.js";
+import { functions, inngest } from "./config/inngest.js";
 const app=express();
 const __dirname=path.resolve();
+app.use(express.json());
+app.use(clerkMiddleware());
+app.use("/api/inngest", serve({ client: inngest, functions }));
 app.get("/api/health",(req,res)=>{
     res.status(200).send("OK");
 })
@@ -15,6 +21,11 @@ if(ENV.NODE_ENV==="production"){
    });
 }
 
-app.listen(ENV.PORT,()=>{
-    console.log("server is running on port 3000");
-})
+const startServer = async () => {
+  await connectDB();
+  app.listen(ENV.PORT, () => {
+    console.log("Server is up and running");
+  });
+};
+
+startServer();
