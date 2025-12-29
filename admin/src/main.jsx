f7a7dcbaf72d179955a8900af3ecbc36bebdaaa6
utsav-kumar7856891309 <15102004utsavkumar@gmail.com>
2025-12-29 21:49:14 +0530

    major changes

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ClerkProvider } from "@clerk/clerk-react";
 import { BrowserRouter } from "react-router";
 
@@ -10,11 +11,14 @@ const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
 }
+const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+        <QueryClientProvider client={queryClient}>
           <App />
+        </QueryClientProvider>
       </ClerkProvider>
     </BrowserRouter>
   </StrictMode>
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,17 +3,30 @@ import path from "path";
 import { clerkMiddleware } from "@clerk/express";
 import { ENV } from "./config/env.js";
 import { serve } from "inngest/express";
+import cors from "cors";
 import { connectDB } from "./config/db.js";
 import { functions, inngest } from "./config/inngest.js";
+import adminRoutes from "./routes/admin.route.js";
+import userRoutes from "./routes/user.route.js";
+import orderRoutes from "./routes/order.route.js";
+import reviewRoutes from "./routes/review.route.js";
+import productRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
 const app=express();
 const __dirname=path.resolve();
 app.use(express.json());
 app.use(clerkMiddleware());
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 app.use("/api/inngest", serve({ client: inngest, functions }));
 app.get("/api/health",(req,res)=>{
     res.status(200).send("OK");
 })
-
+app.use("/api/admin", adminRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/reviews", reviewRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
 if(ENV.NODE_ENV==="production"){
     app.use(express.static(path.join(__dirname,"../admin/dist")));
     app.get("/{*any}", (req, res) => {
