9c04f442ca05b2dd7d5b2890cb71999c8022d06c
utsav-kumar7856891309 <15102004utsavkumar@gmail.com>
2025-12-30 22:08:25 +0530

    mobile setup

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,24 +1,15 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
-
+import { Stack } from "expo-router";
+import "../global.css"
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import { ClerkProvider } from '@clerk/clerk-expo'
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
+const queryClient = new QueryClient();
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <ClerkProvider tokenCache={tokenCache}>
+      <QueryClientProvider client={queryClient}>
+        <Stack screenOptions={{headerShown:false}}/>
+      </QueryClientProvider>
+    </ClerkProvider>
   );
 }
